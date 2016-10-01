819490f8c8948e455ce0d083cc2714acba17cd97
aliouneriddle <aliouneriddle@yahoo.fr>
2016-10-01 01:25:58 +0000

    Update server.R

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 function(input, output) {
+  output$text <- renderText({paste0("You are viewing tab \"", input$tabs, "\"")})
   output$cours <- renderPrint({input$coursha})
-  output$nb <- renderPrint({input$nbre})
+  output$nego <- renderPrint(1 + (input$taf*input$btaf)+ input$neg)
   output$achat <- renderPrint({input$nbre*input$coursha*1.01587})
-  output$rslt <- renderPrint({(input$nbre*input$coursvte*0.98413)-(input$nbre*input$coursha*1.01587)})
+  output$pl <- renderPlot({plot(rnorm(input$coursha),type="b")})
 }
