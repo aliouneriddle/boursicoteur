05159ea58438bdbafd26c9ea4b35a09a43e5d747
aliouneriddle <aliouneriddle@yahoo.fr>
2016-10-01 01:30:06 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,20 +1,23 @@
-fluidPage(
+basicPage(
   titlePanel("Calculs Boursiers !"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(inputId = "coursha",
-                  label = "Cours de l'action",
-                  value = 45, min = 20, max = 70,step=1),
-      numericInput("nbre", "Nbre d'action", 45500, 40, max = 100000, step = 1,width = NULL),
-    sliderInput(inputId = "coursvte",
-                label = "Cours de l'action",
-                value = 45, min = 20, max = 70,step=1)),
-#  sliderInput(inputId = "nbre",
-#              label = "Nombre d'actions",
-#              value = 25, min = 1, max = 50000),
-
-  mainPanel(
-    verbatimTextOutput("cours"),verbatimTextOutput("nb"),
-    verbatimTextOutput("achat"),verbatimTextOutput("rslt")
-    )
+  sidebarPanel(
+    sliderInput(inputId = "coursha",label = "Cours achat",value = 45, min = 20, max = 70,step=1),
+    numericInput("nbre","Nbre d'action",45500,40,100000,1,width=NULL),
+    sliderInput(inputId = "coursvte",label = "Cours vente",value = 45, min = 20, max = 70,step=1)
+    ),
+  
+mainPanel(
+  tabsetPanel(id = "tabs",
+              tabPanel("Paramètres", value = "A", hr(),
+                       numericInput("neg", "Négociation", 0.014, 0, 1, step = 0.001,width = NULL),
+                       numericInput("val", "Valorisation", 0.025, 0,1, step = 0.001,width = NULL),
+                       numericInput("taf", "TAF", 0.17, 0, 1, 0.01,width = NULL),
+                       numericInput("btaf", "Base TAF", 0.011, 0, 1, 0.001,width = NULL),
+                       verbatimTextOutput("nego")),
+              tabPanel("Simulation", value = "B", br(),br(),"Here's some content for tab B."),
+              tabPanel("Prévisions", value = "C", br(),br(),
+                       numericInput("val", "Valorisation", 0.025, 0,1, step = 0.001,width = NULL)),
+              tabPanel("Panorama", value = "D", br(),br(),verbatimTextOutput("cours")),
+              tabPanel("Graphiques", value = "E", br(),br(),plotOutput("pl"))
+              )
 ))
